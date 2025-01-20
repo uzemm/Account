2799fe18a87912a2fdbcd6218305fbe98e5dffd3
yujin <100543186+uzemm@users.noreply.github.com>
2025-01-21 01:05:05 +0900

    Feat: 계좌 확인 API 완성

---
+++ src/main/java/com/example/account/dto/AccountInfo.java
@@ -0,0 +1,14 @@
+package com.example.account.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountInfo {
+
+    private String accountNumber;
+    private Long balance;
+}
