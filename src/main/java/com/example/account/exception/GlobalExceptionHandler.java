0b9cebf75d8cb5f849de93f3bcb480c319de120d
yujin <100543186+uzemm@users.noreply.github.com>
2025-01-26 16:26:05 +0900

    Feat: 일관성 있는 예외처리 완성
    -에러 응답을 공통화하고 원하는 구조의 errorMessage를 만들어줌

---
+++ src/main/java/com/example/account/exception/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.example.account.exception;
+
+import com.example.account.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.example.account.type.ErrorCode.INTERNAL_SERVER_ERROR;
+import static com.example.account.type.ErrorCode.INVALID_REQUEST;
+
+@Slf4j
+@RestControllerAdvice //글로벌하게 발생하는 모든 Exception을 처리
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccountException.class)
+    public ErrorResponse handleAccountException(AccountException e) {
+        log.error("{} is occurred.", e.getErrorCode());
+
+        return new ErrorResponse(e.getErrorCode(), e.getErrorMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException is occurred.", e);
+
+        return new ErrorResponse(INVALID_REQUEST, INVALID_REQUEST.getDescription());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        log.error("DataIntegrityViolationException is occurred.", e);
+
+        return new ErrorResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR.getDescription());
+    }
+
+    @ExceptionHandler(Exception.class)//다른 Exception이 발생할 수도 있으니까
+    public ErrorResponse handleException(Exception e) {
+        log.error("exception is occurred.", e);
+
+        return new ErrorResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR.getDescription());
+    }
+}
--- src/test/java/com/example/account/controller/AccountControllerTest.java
+++ src/test/java/com/example/account/controller/AccountControllerTest.java
@@ -4,9 +4,11 @@ import com.example.account.domain.Account;
 import com.example.account.dto.AccountDto;
 import com.example.account.dto.CreateAccount;
 import com.example.account.dto.DeleteAccount;
+import com.example.account.exception.AccountException;
 import com.example.account.type.AccountStatus;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
+import com.example.account.type.ErrorCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -133,5 +135,19 @@ class AccountControllerTest {
                 .andExpect(jsonPath("$[1].balance").value(2000))
                 .andExpect(jsonPath("$[2].accountNumber").value("2222222222"))
                 .andExpect(jsonPath("$[2].balance").value(3000));
+    }
+
+    @Test
+    void failGetAccount() throws Exception {
+        //given
+        given(accountService.getAccount(anyLong()))
+                .willThrow(new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+        //when
+        //then
+        mockMvc.perform(get("/account/897"))
+                .andDo(print())
+                .andExpect(jsonPath("$.errorCode").value("ACCOUNT_NOT_FOUND"))
+                .andExpect(jsonPath("$.errorMessage").value("계좌가 없습니다."))
+                .andExpect(status().isOk());
     }
 }
